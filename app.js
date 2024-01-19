e9f7a44e2b2cb1b10085ec7843ccfb19844732c6
Ashwanth2310 <141632834+Ashwanth2310@users.noreply.github.com>
2024-01-19 10:44:12 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+let arr = [];
+
+// Progression 1: create a function and fetch the api using axios
+function getData() {
+  const getBlog = document.getElementById('blog');
+
+  axios
+    .get(
+      'https://gnews.io/api/v4/search?q=example&token=b8284089f1792b251b16e49127af5b95&lang=en'
+    )
+    .then((res) => {
+      const listOfArticles = res.data.articles;
+      
+      listOfArticles.forEach((article) => {
+        const getArticle = document.createElement('div');
+        getArticle.setAttribute('id', 'article');
+        getArticle.setAttribute('class', 'article');
+
+        const title = document.createElement('h3');
+        const content = document.createElement('p');
+        const img = document.createElement('img');
+
+        title.innerText = article.title;
+        content.innerText = article.content;
+        img.setAttribute('src', article.image);
+        
+        getArticle.append(title);
+        getArticle.append(img);
+        getArticle.append(content);
+
+        arr.push(getArticle);
+        
+      });
+      arr.forEach((ele) => {
+        getBlog.append(ele);
+
+    });
+  });
+}
+
+getData();
